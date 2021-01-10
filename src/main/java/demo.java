7080afa23ce133f620295af59f91bdfecbe9c279
SUMYAK JAIN <3456gaurav@gmail.com>
2021-01-10 22:05:21 +0530

    Initial commit

---
+++ src/main/java/demo.java
@@ -0,0 +1,12 @@
+public class demo {
+    public static void main(String args[]) {
+        int a = 9;
+        int b = 2;
+        if (a > b) {
+            System.out.println("a is greater");
+        }
+        else{
+            System.out.println("b is greater");
+        }
+    }
+}
